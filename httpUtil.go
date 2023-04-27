733b628e6e207d7dba9de9ca43352f1aa79928ba
ifacker <2677863724@qq.com>
2023-04-27 14:51:52 +0800

    update v1.1.4

--- httpUtil.go
+++ httpUtil.go
@@ -1,10 +1,14 @@
 package myutil
 
 import (
+	"bufio"
+	"bytes"
 	"compress/gzip"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 )
 
 // 把 http 请求返回的 resp 的 body 读取转换成 byte 类型，如果出现乱码，可能是存在未解压的问题，该方法将自动帮你解压，避免出现乱码问题
@@ -27,4 +31,45 @@ func AutoReaderBody2Byte(resp *http.Response) ([]byte, error) {
 		return body, err
 	}
 	return body, nil
+}
+
+// 扫描 请求包中 Content-Length 的长度
+func ScanBodyContentLength2int(data []byte) int {
+	if data == nil {
+		return 0
+	}
+	var contentLength = 0
+	if bytes.Contains(data, []byte("\r\n\r\n")) {
+		arr := bytes.SplitN(data, []byte("\r\n\r\n"), 2)
+		if len(arr) > 1 {
+			contentLength = len(arr[1])
+		}
+	} else if bytes.Contains(data, []byte("\n\n")) {
+		arr := bytes.SplitN(data, []byte("\n\n"), 2)
+		if len(arr) > 1 {
+			contentLength = len(arr[1])
+		}
+	}
+	return contentLength
+}
+
+// 给我一个 data 数据包，还你一个 request 和 err
+func Data2Request(baseUrl string, data []byte) (*http.Request, error) {
+	if data == nil {
+		return nil, errors.New("data 数据为空")
+	}
+	contentLength := ScanBodyContentLength2int(data)
+	req, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(data)))
+	if err != nil {
+		return nil, err
+	}
+	req.RequestURI = ""
+	req.ContentLength = int64(contentLength)
+	fmt.Println(req.URL)
+	u, err := url.Parse(baseUrl)
+	if err != nil {
+		return nil, err
+	}
+	req.URL = u
+	return req, nil
 }
--- httpUtil_test.go
+++ httpUtil_test.go
@@ -1,103 +1,18 @@
-## 小工具类
+package myutil
 
-### AutoReaderBody2Byte
-把 http 请求返回的 resp 的 body 读取转换成 byte 类型，如果出现乱码，可能是存在未解压的问题，该方法将自动帮你解压，避免出现乱码问题
-```go
-	client := &http.Client{}
-	req, err := http.NewRequest(http.MethodGet, sendUrl, nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-	}
-	bodyByte, err := myutil.AutoReaderBody2Byte(resp)  // 划重点
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(string(bodyByte))
-```
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net/http"
+	"testing"
+)
 
-### Unicode2String  
-把 unicode 转为 string
-```go
-	unicode := `\u60a8\u4f3c\u4e4e\u5df2\u7ecf\u7b7e\u5230\u8fc7\u4e86..`
-	str, err := Unicode2String(unicode)
-	fmt.Println(str, err)
-```
-
-### StringToBytes
-把 string 转为 byte[]
-```go
-	a := "abc"
-	b := StringToBytes(a)
-	fmt.Println(b)
-```
-
-### BytesToString
-把 []byte 转为 string
-```go
-	a := []byte{'a', 'b', 'c'}
-	b := BytesToString(a)
-	fmt.Println(b)
-```
-
-### NewReadAll
-读取 io.reader 流，并返回 []byte 类型数据
-```go
-	file, err := os.Open("test.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	buf := bufio.NewReader(file)
-	result, _ := NewReadAll(buf)
-	fmt.Println(string(result))
-```
-
-### InitProxy
-设置代理，同时支持并自动识别 socks5 协议和 http 协议  
-传入代理的格式是：  
-socks5://localhost:8080  
-http://localhost:8080  
-```go
-	proxyUrl := "socks5://localhost:1080"
-	//proxyUrl := "http://localhost:1080"
-	tr := &http.Transport{
-		MaxIdleConns:        500,
-		MaxIdleConnsPerHost: 500,
-		MaxConnsPerHost:     500,
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-	}
-	// 为 tr 添加代理
-	err := InitProxy(tr, proxyUrl)
-	if err != nil {
-		fmt.Println(err)
-	}
-	clinet := http.Client{
-		Transport: tr,
-	}
-	req, err := http.NewRequest("GET", "https://baidu.com", nil)
-	if err != nil {
-		log.Println(err)
-	}
-	resp, err := clinet.Do(req)
-	if err != nil {
-		log.Println(err)
-	}
-	result, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(result))
-```
-
-### ScanBodyContentLength2int
-传一个数据包进去，给你返回数据包中 body 的长度  
-这个方法适用于请求包中没有 Content-Length 参数，但是是 POST 请求，这个时候用来计算 body 长度的
-```go
+func TestScanBodyContentLength2int(t *testing.T) {
 	str := `POST /mcp/pc/pcsearch HTTP/1.1
 Host: ug.baidu.com
 Cookie: BIDUPSID=DE728E5577FBF6CE396E7A9B6EB9FF7E; PSTM=1666859102; BAIDUID=DE728E5577FBF6CEDB3E4C42CE7D2961:FG=1; BAIDUID_BFESS=DE728E5577FBF6CEDB3E4C42CE7D2961:FG=1; ZFY=j7:Bn4Ni084v1YF9ehv2QDaQJ:AXEkMTRZXWfvL8qzAWA:C; H_PS_PSSID=38515_36547_38529_38469_38350_38368_38468_38486_37935_26350_38545; BA_HECTOR=ap8ha0852ka00l858l2ha0fe1i4jp8s1m; BDRCVFR[S4-dAuiWMmn]=I67x6TjHwwYf0; delPer=0; PSINO=5; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598
+Content-Length: 56
 Sec-Ch-Ua: "Chromium";v="103", ".Not/A)Brand";v="99"
 Sec-Ch-Ua-Mobile: ?0
 User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36
@@ -116,15 +31,29 @@ Connection: close
 {"invoke_info":{"pos_1":[{}],"pos_2":[{}],"pos_3":[{}]}}`
 	a := ScanBodyContentLength2int([]byte(str))
 	fmt.Println(a)
-```
+}
 
-### Data2Request
-给我一个 data 数据包，还你一个 request 和 err  
-一般 http.ReadRequest 这个方法读取的数据包，有的时候是没办法直接用的，这个时候使用本方法，可以完美的修复了因为缺少 Content-Length 参数而无法使用的数据包
-```go
+func TestData2Request(t *testing.T) {
+
+	proxyUrl := "http://localhost:8080"
+	tr := &http.Transport{
+		MaxIdleConns:        500,
+		MaxIdleConnsPerHost: 500,
+		MaxConnsPerHost:     500,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+	// 为 tr 添加代理
+	err := InitProxy(tr, proxyUrl)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	str := `POST /mcp/pc/pcsearch HTTP/1.1
 Host: ug.baidu.com
 Cookie: BIDUPSID=DE728E5577FBF6CE396E7A9B6EB9FF7E; PSTM=1666859102; BAIDUID=DE728E5577FBF6CEDB3E4C42CE7D2961:FG=1; BAIDUID_BFESS=DE728E5577FBF6CEDB3E4C42CE7D2961:FG=1; ZFY=j7:Bn4Ni084v1YF9ehv2QDaQJ:AXEkMTRZXWfvL8qzAWA:C; H_PS_PSSID=38515_36547_38529_38469_38350_38368_38468_38486_37935_26350_38545; BA_HECTOR=ap8ha0852ka00l858l2ha0fe1i4jp8s1m; BDRCVFR[S4-dAuiWMmn]=I67x6TjHwwYf0; delPer=0; PSINO=5; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598
+Content-Length: 56
 Sec-Ch-Ua: "Chromium";v="103", ".Not/A)Brand";v="99"
 Sec-Ch-Ua-Mobile: ?0
 User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36
@@ -145,7 +74,9 @@ Connection: close
 	if err != nil {
 		log.Println(err)
 	}
-	client := &http.Client{}
+	client := &http.Client{
+		Transport: tr,
+	}
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Println(err)
@@ -155,18 +86,4 @@ Connection: close
 		log.Println(err)
 	}
 	fmt.Println(string(a))
-```
-
----
-### v1.1.4
-新增了两个方法
-1. ScanBodyContentLength2int
-2. Data2Request
-
-### v1.1.3
-修复了 AutoReaderBody2Byte 方法的 bug
-
-### v1.1.2
-新更新的版本处理了各个方法的空指针异常返回报错
-
-...
+}
